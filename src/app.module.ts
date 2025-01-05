dea5a6a6d9a4625097b91756ce3b52dc26402c0c
Katielly <katiellyscherbatsky@gmail.com>
2025-01-05 13:56:25 -0400

    fix: app module importacoes typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,21 @@ import { AuthModule } from './auth/auth.module';
 import { Alarme } from './entities/alarme.entity';
 import { Alimentacao } from './entities/alimentacao.entity';
 import { Comedouro } from './entities/comedouro.entity';
+import { Fornecedor } from './entities/fornecedor.entity';
 import { Pedido } from './entities/pedido.entity';
 import { Pet } from './entities/pet.entity';
 import { Produto } from './entities/produto.entity';
+import { ProdutoFornecedor } from './entities/produto_fornecedor.entity';
+import { Usuario } from './entities/usuario.entity';
 import { AlarmeModule } from './modules/alarme/alarme.module';
 import { AlimentacaoModule } from './modules/alimentacao/alimentacao.module';
 import { ComedouroModule } from './modules/comedouro/comedouro.module';
+import { FornecedorModule } from './modules/fornecedor/fornecedor.module';
 import { PedidoModule } from './modules/pedido/pedido.module';
 import { PetModule } from './modules/pet/pet.module';
 import { ProdutoModule } from './modules/produto/produto.module';
+import { ProdutoFornecedorModule } from './modules/produto_fornecedor/produto_fornecedor.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
 
 @Module({
   imports: [
@@ -25,16 +31,19 @@ import { ProdutoModule } from './modules/produto/produto.module';
       username: 'postgres.emubutxtgghgqhmsacqh',
       password: 'Comedouro123456@',
       database: 'postgres',
-      entities: [Pet, Alarme, Alimentacao, Comedouro, Produto, Pedido],
+      entities: [Alarme, Alimentacao, Comedouro, Fornecedor, Pedido, Pet,Produto, ProdutoFornecedor, Usuario],
       synchronize: false,
     }),
-    TypeOrmModule.forFeature([Pet, Alarme, Alimentacao, Comedouro, Produto, Pedido]),
-    PetModule,
+    TypeOrmModule.forFeature([Alarme, Alimentacao, Comedouro,  Fornecedor, Pedido, Pet, Produto, ProdutoFornecedor, Usuario ]),
     AlarmeModule,
     AlimentacaoModule,
     ComedouroModule,
-    ProdutoModule,
+    FornecedorModule,
     PedidoModule,
+    PetModule,
+    ProdutoModule,
+    ProdutoFornecedorModule,
+    UsuarioModule,
     AuthModule,
   ],
   controllers: [AppController],
